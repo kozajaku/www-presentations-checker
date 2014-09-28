56610c7cf285013dad7d8c994a24c84f6549bcc7
Jindřich Máca <macajind@fit.cvut.cz>
2014-09-29 00:35:10 +0200

    Nový create script
    Vygeneroval jsem a upravil nový create script a ještě jsem upravil v DB
    modelu FK na CASCADE.

--- Development/Implementation/create_script.sql
+++ Development/Implementation/create_script.sql
@@ -10,8 +10,11 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 
 DROP TABLE IF EXISTS `checkup`;
+DROP TABLE IF EXISTS `checkup_has_option`;
 DROP TABLE IF EXISTS `domain`;
+DROP TABLE IF EXISTS `graph`;
 DROP TABLE IF EXISTS `login`;
+DROP TABLE IF EXISTS `option`;
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `checkup`
@@ -20,17 +23,23 @@ CREATE TABLE `checkup`
 	`checking_created` DATETIME NOT NULL,
 	`state` VARCHAR(50) NOT NULL,
 	`result_log` MEDIUMTEXT,
-	`graph` MEDIUMTEXT,
 	`checking_finished` DATETIME,
 	`start_point` VARCHAR(255) NOT NULL,
 	`max_depth` INTEGER,
-	`check_HTML` BOOL NOT NULL,
-	`check_CSS` BOOL NOT NULL,
-	`check_CSS_redundancy` BOOL NOT NULL,
-	`check_links` BOOL NOT NULL,
-	`user` INTEGER NOT NULL,
+	`user` VARCHAR(254) NOT NULL,
+	`checking_interval` INTEGER,
 	PRIMARY KEY (`id_checkup`),
 	KEY (`user`)
+) ENGINE = InnoDB;
+
+
+CREATE TABLE `checkup_has_option`
+(
+	`checkup` INTEGER NOT NULL,
+	`option` VARCHAR(50) NOT NULL,
+	PRIMARY KEY (`checkup`, `option`),
+	KEY (`checkup`),
+	KEY (`option`)
 ) ENGINE = InnoDB;
 
 
@@ -38,10 +47,19 @@ CREATE TABLE `domain`
 (
 	`id_domain` INTEGER NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(255) NOT NULL,
-	`request_interval` INTEGER,
 	`checking` INTEGER NOT NULL,
 	PRIMARY KEY (`id_domain`),
 	KEY (`checking`)
+) ENGINE = InnoDB;
+
+
+CREATE TABLE `graph`
+(
+	`id_graph` INTEGER NOT NULL,
+	`output` MEDIUMTEXT,
+	`checkup` INTEGER,
+	PRIMARY KEY (`id_graph`),
+	KEY (`checkup`)
 ) ENGINE = InnoDB;
 
 
@@ -50,37 +68,57 @@ CREATE TABLE `login`
 	`id_login` INTEGER NOT NULL AUTO_INCREMENT,
 	`address` VARCHAR(50) NOT NULL,
 	`time` DATETIME NOT NULL,
-	`user` INTEGER NOT NULL,
+	`user` VARCHAR(254) NOT NULL,
 	PRIMARY KEY (`id_login`),
 	KEY (`user`)
 ) ENGINE = InnoDB;
 
 
+CREATE TABLE `option`
+(
+	`option_id` VARCHAR(50) NOT NULL,
+	PRIMARY KEY (`option_id`)
+) ENGINE = InnoDB;
+
+
 CREATE TABLE `user`
 (
-	`id_user` INTEGER NOT NULL AUTO_INCREMENT,
 	`email` VARCHAR(254) NOT NULL,
 	`password` VARCHAR(64) NOT NULL,
 	`salt` VARCHAR(64) NOT NULL,
 	`name` VARCHAR(100) NOT NULL,
 	`surname` VARCHAR(100) NOT NULL,
 	`registration_date` DATE NOT NULL,
-	PRIMARY KEY (`id_user`)
+	PRIMARY KEY (`email`)
 ) ENGINE = InnoDB;
 
+
 ALTER TABLE `checkup` ADD CONSTRAINT `FK_checkup_user` 
-	FOREIGN KEY (`user`) REFERENCES `user` (`id_user`)
+	FOREIGN KEY (`user`) REFERENCES `user` (`email`)
+	ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `checkup_has_option` ADD CONSTRAINT `FK_checkup_has_option_checkup` 
+	FOREIGN KEY (`checkup`) REFERENCES `checkup` (`id_checkup`)
+	ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `checkup_has_option` ADD CONSTRAINT `FK_checkup_has_option_option` 
+	FOREIGN KEY (`option`) REFERENCES `option` (`option_id`)
 	ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE `domain` ADD CONSTRAINT `FK_domain_checkup` 
 	FOREIGN KEY (`checking`) REFERENCES `checkup` (`id_checkup`)
 	ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE `login` ADD CONSTRAINT `FK_login_user` 
-	FOREIGN KEY (`user`) REFERENCES `user` (`id_user`)
+ALTER TABLE `graph` ADD CONSTRAINT `FK_graph_checkup` 
+	FOREIGN KEY (`checkup`) REFERENCES `checkup` (`id_checkup`)
 	ON DELETE CASCADE ON UPDATE CASCADE;
-                    
+
+ALTER TABLE `login` ADD CONSTRAINT `FK_login_user` 
+	FOREIGN KEY (`user`) REFERENCES `user` (`email`)
+	ON DELETE CASCADE ON UPDATE CASCADE;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-                    
+                  
