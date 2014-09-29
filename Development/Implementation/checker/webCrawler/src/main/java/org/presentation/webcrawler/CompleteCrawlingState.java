2bb07a7cf08317a971a7cff96cbeea785a5c107e
Adam Kugler <kugleada@fit.cvut.cz>
2014-09-29 17:26:55 +0200

    Implementace CompleteCrawlingState

---
+++ Development/Implementation/checker/webCrawler/src/main/java/org/presentation/webcrawler/CompleteCrawlingState.java
@@ -0,0 +1,20 @@
+package org.presentation.webcrawler;
+
+/**
+ * @author Adam Kugler
+ * @version 1.0
+ * @created 04-5-2014 20:56:23
+ */
+public enum CompleteCrawlingState {
+	ENDED_BY_DEPTH,
+	ENDED_BY_PAGE_LIMIT,
+	STOPPED_BY_USER,
+	/**
+	 * Error in web crawling (server disconnected, etc...)
+	 */
+	ERROR,
+	/**
+	 * Unknown web crawler completion state - should not be used.
+	 */
+	UNKNOWN
+}
