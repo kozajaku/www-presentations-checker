1497c9ffbffe1f2d92787bbb23a911cf606cc8e0
Adam Kugler <kugleada@fit.cvut.cz>
2014-09-29 17:23:10 +0200

    Přesunutí CrawlingState do spravne složky

---
+++ Development/Implementation/checker/webCrawler/src/main/java/org/presentation/webcrawler/CrawlingState.java
@@ -0,0 +1,33 @@
+package org.presentation.webcrawler;
+
+/**
+ * @author Adam Kugler
+ * @version 1.0
+ * @created 04-5-2014 20:56:23
+ */
+public class CrawlingState {
+
+	private int pagesCrawled;
+	private boolean done;
+
+	public CrawlingState(){
+            pagesCrawled = 0;
+            done = false;
+	}
+
+        public void done(){
+            done = true;
+        }
+        
+        public void incCount(){
+            pagesCrawled++;
+        }
+        
+        public boolean isDone(){
+            return done;
+        }
+        
+        public int getPagesCrawled(){
+            return pagesCrawled;
+        }
+}
