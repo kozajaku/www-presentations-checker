7f4859e63a5a051cd880ce80ce72191163017cb9
Adam Kugler <kugleada@fit.cvut.cz>
2014-09-30 22:02:05 +0200

    Implementace CrawlerService, Domain

---
+++ Development/Implementation/checker/model/src/main/java/org/presentation/model/Domain.java
@@ -0,0 +1,16 @@
+package org.presentation.model;
+
+/**
+ * Model class for web domain description.
+ * @author Adam Kugler
+ * @version 1.0
+ */
+public class Domain {
+
+	private String domain;
+
+    public Domain(String domain) {
+        this.domain = domain;
+    }
+        
+}
---
+++ Development/Implementation/checker/webCrawler/src/main/java/org/presentation/webcrawler/CrawlerService.java
@@ -0,0 +1,38 @@
+package org.presentation.webcrawler;
+import java.util.List;
+import org.presentation.checker.utils.Stoppable;
+import org.presentation.model.Domain;
+import org.presentation.model.LinkURL;
+import org.presentation.model.logging.MessageProducer;
+
+/**
+ * Interface for WebCrawler service. Every module, that want to serve as
+ * WebCrawler service provider must register implementation of this service.
+ * @author Adam Kuger
+ * @version 1.0
+ */
+public interface CrawlerService extends MessageProducer, Stoppable {
+
+	/**
+	 * 
+	 * @param url
+	 * @param maximalDepth
+	 * @param observer    Observer object, into which the CrawlerService
+	 * implementation will send its results.
+	 */
+	public abstract void startBrowsing(LinkURL url, int maximalDepth, PageCrawlingObserver observer);
+
+	/**
+	 * 
+	 * @param url
+	 * @param maximalDepth
+	 * @param observer    Observer object, into which the CrawlerService
+	 * implementation will send its results.
+	 * @param allowedDomains
+	 * @param requestTimeout
+	 */
+	public abstract void startBrowsing(LinkURL url, int maximalDepth, PageCrawlingObserver observer, List<Domain> allowedDomains, int requestTimeout);
+
+	public CrawlingState getCrawlingState();
+
+}
