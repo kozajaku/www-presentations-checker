0ea1b36c7d116f7b56a1e8e761744adfaff2cf96
Adam Kugler <kugleada@fit.cvut.cz>
2014-10-12 13:49:14 +0200

    příprava GraphGenerator

---
+++ Development/Implementation/checker/graphGenerator/src/main/java/org/presentation/graphgenerator/GraphGenerator.java
@@ -0,0 +1,11 @@
+package org.presentation.graphgenerator;
+
+import org.presentation.model.graph.TraversalGraph;
+
+/**
+ *
+ * @author Adam Kugler
+ */
+public abstract class GraphGenerator {
+    public abstract GraphResult generateGraphResult(TraversalGraph graph);
+}
---
+++ Development/Implementation/checker/graphGenerator/src/main/java/org/presentation/graphgenerator/GraphTextGenerator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.presentation.graphgenerator;
+
+import org.presentation.model.graph.TraversalGraph;
+
+/**
+ *
+ * @author Adam Kugler
+ */
+public class GraphTextGenerator extends GraphGenerator{
+
+    @Override
+    public GraphResult generateGraphResult(TraversalGraph graph) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
