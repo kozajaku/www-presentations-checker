05abf82523465d8ad2d83280a51c02e58d4e920a
Jakub Koza <kozajaku@fit.cvut.cz>
2014-10-19 19:54:14 +0200

    Vytvořen testovací projekt na w3c validator api

---
+++ Development/Implementation/htmlValidatorTest/src/main/java/org/presentation/htmlvalidatortest/Receiver.java
@@ -0,0 +1,16 @@
+package org.presentation.htmlvalidatortest;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author radio.koza
+ */
+public class Receiver {
+    
+    public InputStream getSoapResponse(String pageToTest){
+        //TODO
+        return null;
+    }
+    
+}
