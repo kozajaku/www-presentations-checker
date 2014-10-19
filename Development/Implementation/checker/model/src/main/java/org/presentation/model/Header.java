996826d3a7d4225b95de7edeb2a2281d4801cc2b
Adam Kugler <kugleada@fit.cvut.cz>
2014-10-20 00:12:00 +0200

    Dokumentace PersistanceFacade

--- Development/Implementation/checker/model/src/main/java/org/presentation/model/Header.java
+++ Development/Implementation/checker/model/src/main/java/org/presentation/model/Header.java
@@ -1,14 +1,18 @@
 package org.presentation.model;
 
 /**
- *
+ * This class represents header in http or https request.
  * @author radio.koza
  */
 public class Header {
 
     private final String key;
     private final String value;
-
+    /**
+     * 
+     * @param key Header key
+     * @param value Header value
+     */
     public Header(String key, String value) {
         this.key = key;
         this.value = value;
