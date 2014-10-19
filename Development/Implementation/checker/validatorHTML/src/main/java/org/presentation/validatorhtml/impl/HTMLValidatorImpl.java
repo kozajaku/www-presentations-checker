2d0d3f3c6bf7e06145814840817ea2b08ebbbde5
Jakub Koza <kozajaku@fit.cvut.cz>
2014-10-19 21:48:25 +0200

    Implementace validátoru html
    Přidal jsem rozhraní pro validaci html pomocí vlastní parsování soap
    odpovědi JAXB frameworkem.

--- Development/Implementation/checker/validatorHTML/src/main/java/org/presentation/validatorhtml/impl/HTMLValidatorImpl.java
+++ Development/Implementation/checker/validatorHTML/src/main/java/org/presentation/validatorhtml/impl/HTMLValidatorImpl.java
@@ -9,12 +9,23 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import org.presentation.model.ContentType;
 import org.presentation.model.LinkURL;
 import org.presentation.model.PageContent;
 import org.presentation.model.logging.MessageLogger;
 import org.presentation.model.logging.MessageLoggerContainer;
 import org.presentation.singlepagecontroller.SinglePageControllerService;
+import org.w3.markup_validator.ErrorList;
+import org.w3.markup_validator.MarkupValidationResponse;
+import org.w3.markup_validator.ValidationErrors;
+import org.w3.markup_validator.ValidationWarnings;
+import org.w3.markup_validator.Warning;
+import org.w3.markup_validator.WarningList;
+import org.w3.soap_envelope.Envelope;
 
 /**
  * HTML validator service.
@@ -47,14 +58,55 @@ public class HTMLValidatorImpl implements SinglePageControllerService {
         return connection.getInputStream();
     }
 
+    private void processMVR(MarkupValidationResponse mvr){
+        //TODO misto System.out.println naper co je potreba do tech loggeru - snad je rozhrani jasne
+
+        //uri
+        System.out.println("URI: " + mvr.getUri());
+        //charset
+        System.out.println("Charset: " + mvr.getCharset());
+        //checked by
+        System.out.println("Checked by: " + mvr.getCheckedby());
+        //doctype
+        System.out.println("Doctype: " + mvr.getDoctype());
+        //validity
+        System.out.println("Validity: " + Boolean.toString(mvr.isValidity()));
+        //errors
+        ValidationErrors errors = mvr.getErrors();
+        System.out.println("Errors count: " + errors.getErrorcount());
+        ErrorList elist = errors.getErrorlist();
+        for (org.w3.markup_validator.Error i: elist.getError()){
+            System.out.println("Error: " + i.getMessage());
+        }
+        //warnings
+        ValidationWarnings warnings = mvr.getWarnings();
+        System.out.println("Warnings count: " + warnings.getWarningcount());
+        WarningList wlist = warnings.getWarninglist();
+        for (Warning w: wlist.getWarning()){
+            System.out.println("Warning: " + w.getMessage());
+        }
+    }
+    
     @Override
     public void checkPage(ContentType contentType, LinkURL url, PageContent text) {
         LOG.log(Level.INFO, "Checking validity of html {0}", url.getUrl());
         try {
-            InputStream input = getSOAPInputStream(url.getUrl());
-            //implement soap representation
+            InputStream stream = getSOAPInputStream(url.getUrl());
+            JAXBContext context = JAXBContext.newInstance("org.w3.soap_envelope:org.w3.markup_validator", this.getClass().getClassLoader());
+            Unmarshaller un = context.createUnmarshaller();
+            Envelope envelope = (Envelope) ((JAXBElement<?>) un.unmarshal(stream)).getValue();
+            for (Object i : envelope.getBody().getAny()) {
+                i = ((JAXBElement<?>) i).getValue();
+                if (i instanceof MarkupValidationResponse) {
+                    MarkupValidationResponse mvr = (MarkupValidationResponse) i;
+                    processMVR(mvr);
+                    return;//done successfully
+                }
+            }
         } catch (IOException ex) {
             LOG.log(Level.WARNING, "{0}", ex.getMessage());
+        } catch (JAXBException ex) {
+            LOG.log(Level.SEVERE, "unable to initialize JAXB parser", ex);
         }
     }
 
