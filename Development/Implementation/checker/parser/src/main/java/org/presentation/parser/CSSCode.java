722a58b8731d2a152a4124130a6641db3e727191
Adam Kugler <kugleada@fit.cvut.cz>
2014-10-25 17:31:19 +0200

    Příprava potřebných tříd pro WPC

---
+++ Development/Implementation/checker/parser/src/main/java/org/presentation/parser/CSSCode.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.presentation.parser;
+
+import org.presentation.model.LinkURL;
+import org.presentation.model.PageContent;
+
+/**
+ * This class is CSS code holder with possibility to parse.
+ *
+ * @author Adam
+ */
+public class CSSCode {
+
+    private PageContent codeCSS;
+    private LinkURL linkCSS;
+    private ? parsedCSS ;
+
+    /**
+     * Creates {@link CSSCode} without parsing.
+     *
+     * @param codeCSS CSS source code
+     * @param linkCSS Link to CSS page
+     * @see #parse()
+     */
+    public CSSCode(PageContent codeCSS, LinkURL linkCSS) {
+        this.codeCSS = codeCSS;
+        this.linkCSS = linkCSS;
+        parsedCSS = null;
+    }
+
+    /**
+     * Parses {@link PageContent} into {@link }. Don't call this method if you
+     * don't need parsed code.
+     */
+    public void parse() {
+        //TO DO
+    }
+}
