bf34b8ad5f98ad155b47979c860d96c9b3f6a84d
Jakub Koza <kozajaku@fit.cvut.cz>
2014-10-31 17:29:08 +0100

    Přidání komentářů do části kernelu

--- Development/Implementation/checker/kernel/src/main/java/org/presentation/kernel/impl/CheckingExecutor.java
+++ Development/Implementation/checker/kernel/src/main/java/org/presentation/kernel/impl/CheckingExecutor.java
@@ -3,7 +3,6 @@ package org.presentation.kernel.impl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
