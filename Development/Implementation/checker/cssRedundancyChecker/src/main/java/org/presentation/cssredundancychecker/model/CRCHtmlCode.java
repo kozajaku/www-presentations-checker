b5b9160b2e5f661fff2ad1dfa7b3b79608f4c367
Jindřich Máca <macajind@fit.cvut.cz>
2014-11-03 00:44:35 +0100

    Oprava CRCHtmlCode

--- Development/Implementation/checker/cssRedundancyChecker/src/main/java/org/presentation/cssredundancychecker/model/CRCHtmlCode.java
+++ Development/Implementation/checker/cssRedundancyChecker/src/main/java/org/presentation/cssredundancychecker/model/CRCHtmlCode.java
@@ -5,7 +5,6 @@
  */
 package org.presentation.cssredundancychecker.model;
 
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -13,7 +12,6 @@ import java.util.logging.Logger;
 import javax.inject.Inject;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.presentation.cssredundancychecker.CSSRedundancyChecker;
 import org.presentation.model.LinkURL;
 import org.presentation.parser.HTMLCode;
 
@@ -26,45 +24,47 @@ public class CRCHtmlCode {
     @Inject
     @SuppressWarnings("NonConstantLogger")
     private Logger LOG;
-    
+
     protected List<LinkURL> stylesheetFilesRequired;
     protected HTMLCode htmlCode;
 
     public CRCHtmlCode(HTMLCode htmlCode) {
-	this.htmlCode = htmlCode;	
-	this.stylesheetFilesRequired = new ArrayList<>();
-	this.loadStylesheetsRequiredFromDocument();
+        this.htmlCode = htmlCode;
+        this.stylesheetFilesRequired = new ArrayList<>();
+        this.loadStylesheetsRequiredFromDocument();
     }
-    
+
     public HTMLCode getHtmlCode() {
-	return htmlCode;
-    }        
+        return htmlCode;
+    }
 
     public List<LinkURL> getStylesheetFilesRequired() {
-	return stylesheetFilesRequired;
+        return stylesheetFilesRequired;
     }
-        
+
     /**
-     * This method retrieves required stylesheet URLs from the document.
-     * The URLs are retrieved from &lt;link&gt; tags.
+     * This method retrieves required stylesheet URLs from the document. The
+     * URLs are retrieved from &lt;link&gt; tags.
      */
     private void loadStylesheetsRequiredFromDocument() {
-	Elements links = this.htmlCode.getParsedHTML().select("link[href]");
-	for (Element link : links) {
+        Elements links = this.htmlCode.getParsedHTML().select("link[href]");
+        for (Element link : links) {
             String type = link.attr("type");
-	    String rel = link.attr("rel");
-	    if((type != null && (type.toLowerCase().equals("text/css"))) || (rel != null && (rel.toLowerCase().equals("stylesheet")))) {
-		String cssHref = link.attr("abs:href");		
-		if(cssHref != null) {
-		    cssHref = Paths.get(cssHref).normalize().toString().replace(":/", "://");  // normalize the path (ex http://example.com/./style.css -> http://example.com/style.css)
-		    LinkURL stylesheetResource = new LinkURL(cssHref.split("#")[0]);
-		    if(LOG != null) LOG.log(Level.INFO, "CSSRC - found CSS: {0}", stylesheetResource.getUrl());	    		
-		    if (stylesheetResource.checkURL()) {
-			this.stylesheetFilesRequired.add(stylesheetResource);
-		    }
-		}
-	    }
+            String rel = link.attr("rel");
+            if ((type != null && (type.toLowerCase().equals("text/css"))) || (rel != null && (rel.toLowerCase().equals("stylesheet")))) {
+                String cssHref = link.attr("abs:href");
+                if (cssHref != null) {
+                    cssHref = cssHref.replaceAll("[/][.]+", "").replace(":/", "://");  // normalize the path (ex http://example.com/./style.css -> http://example.com/style.css)
+                    LinkURL stylesheetResource = new LinkURL(cssHref.split("#")[0]);
+                    if (LOG != null) {
+                        LOG.log(Level.INFO, "CSSRC - found CSS: {0}", stylesheetResource.getUrl());
+                    }
+                    if (stylesheetResource.checkURL()) {
+                        this.stylesheetFilesRequired.add(stylesheetResource);
+                    }
+                }
+            }
         }
     }
-    
+
 }
